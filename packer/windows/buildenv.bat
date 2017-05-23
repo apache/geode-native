cb9c4e21f696236e84d9228874a307083b5788ba
Jacob Barrett <jbarrett@pivotal.io>
2017-05-23 08:05:19 -0700

    GEODE-2741: Update Windows build to use Visual Studio 2015 / VC14.

--- packer/windows/buildenv.bat
+++ packer/windows/buildenv.bat
@@ -21,5 +21,5 @@ set TMP=C:\Users\build\AppData\Local\Temp\3
 if not exist %TEMP% mkdir %TEMP%
 
 @rem Setup VC
-call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" %*
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" %*
 
