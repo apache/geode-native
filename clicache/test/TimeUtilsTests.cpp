19a611759fa4eeb705f31b3cbd3d360bb78b3e3d
Jacob Barrett <jbarrett@pivotal.io>
2017-11-15 16:11:28 +0000

    GEODE-3392: Replace tabs with spaces.

--- clicache/test/TimeUtilsTests.cpp
+++ clicache/test/TimeUtilsTests.cpp
@@ -31,54 +31,54 @@ using namespace Apache::Geode::Client;
 
 namespace cliunittests
 {
-	[TestClass]
-	public ref class TimeUtilsTests
-	{
-	private:
-		TestContext^ testContextInstance;
+  [TestClass]
+  public ref class TimeUtilsTests
+  {
+  private:
+    TestContext^ testContextInstance;
 
-	public: 
-		/// <summary>
-		///Gets or sets the test context which provides
-		///information about and functionality for the current test run.
-		///</summary>
-		property Microsoft::VisualStudio::TestTools::UnitTesting::TestContext^ TestContext
-		{
-			Microsoft::VisualStudio::TestTools::UnitTesting::TestContext^ get()
-			{
-				return testContextInstance;
-			}
-			System::Void set(Microsoft::VisualStudio::TestTools::UnitTesting::TestContext^ value)
-			{
-				testContextInstance = value;
-			}
-		};
+  public: 
+    /// <summary>
+    ///Gets or sets the test context which provides
+    ///information about and functionality for the current test run.
+    ///</summary>
+    property Microsoft::VisualStudio::TestTools::UnitTesting::TestContext^ TestContext
+    {
+      Microsoft::VisualStudio::TestTools::UnitTesting::TestContext^ get()
+      {
+        return testContextInstance;
+      }
+      System::Void set(Microsoft::VisualStudio::TestTools::UnitTesting::TestContext^ value)
+      {
+        testContextInstance = value;
+      }
+    };
 
-		#pragma region Additional test attributes
-		//
-		//You can use the following additional attributes as you write your tests:
-		//
-		//Use ClassInitialize to run code before running the first test in the class
-		//[ClassInitialize()]
-		//static void MyClassInitialize(TestContext^ testContext) {};
-		//
-		//Use ClassCleanup to run code after all tests in a class have run
-		//[ClassCleanup()]
-		//static void MyClassCleanup() {};
-		//
-		//Use TestInitialize to run code before running each test
-		//[TestInitialize()]
-		//void MyTestInitialize() {};
-		//
-		//Use TestCleanup to run code after each test has run
-		//[TestCleanup()]
-		//void MyTestCleanup() {};
-		//
-		#pragma endregion 
+    #pragma region Additional test attributes
+    //
+    //You can use the following additional attributes as you write your tests:
+    //
+    //Use ClassInitialize to run code before running the first test in the class
+    //[ClassInitialize()]
+    //static void MyClassInitialize(TestContext^ testContext) {};
+    //
+    //Use ClassCleanup to run code after all tests in a class have run
+    //[ClassCleanup()]
+    //static void MyClassCleanup() {};
+    //
+    //Use TestInitialize to run code before running each test
+    //[TestInitialize()]
+    //void MyTestInitialize() {};
+    //
+    //Use TestCleanup to run code after each test has run
+    //[TestCleanup()]
+    //void MyTestCleanup() {};
+    //
+    #pragma endregion 
 
-		[TestMethod]
-		void TimePointToDateTime()
-		{
+    [TestMethod]
+    void TimePointToDateTime()
+    {
       auto testSystemEpoch = DateTime(1970, 1, 1, 0, 0, 0, 0, gcnew System::Globalization::GregorianCalendar(), DateTimeKind::Utc);
       Assert::AreEqual(621355968000000000, testSystemEpoch.Ticks);
 
@@ -91,12 +91,12 @@ namespace cliunittests
       Assert::AreEqual(netNextDay, convertedNextDay);
     }
 
-		[TestMethod]
-		void DateTimeToTimePoint()
-		{
+    [TestMethod]
+    void DateTimeToTimePoint()
+    {
       const auto netEpoch = TimeUtils::DateTimeToTimePoint(DateTime(0));
       Assert::AreEqual(-621355968000000000, ticks{netEpoch.time_since_epoch()}.count());
-		}
+    }
 
   };
 }
