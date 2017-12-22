a2233715af2a587b978672eb532ed5aaef4ec26e
Jacob Barrett <jbarrett@pivotal.io>
2017-12-22 08:36:15 -0800

    GEODE-4143: Improves integration test stability. (#172)
    - Fixes test hangs on Windows.
    - Fixes crashes on exception.
    - Disables http service in locator startup.

--- clicache/integration-test/CacheHelperN.cs
+++ clicache/integration-test/CacheHelperN.cs
@@ -1990,7 +1990,7 @@ namespace Apache.Geode.Client.UnitTests
         {
           extraLocatorArgs = locatorPort;
         }
-        string locatorArgs = LocatorStartArgs + " --name=" + serverName + startDir + extraLocatorArgs;
+        string locatorArgs = LocatorStartArgs + " --name=" + serverName + startDir + extraLocatorArgs + " --http-service-port=0";
 
         if (!Util.StartProcess(locatorPath, locatorArgs, false, null, true,
           false, false, true, out javaProc))
