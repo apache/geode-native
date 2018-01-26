57adba5ab4ce47947d0de118694a555fa99c8eef
David Kimura <dkimura@pivotal.io>
2018-01-26 15:39:19 -0800

    GEODE-4186: Re-add documentation comments for buildin cacheable types

--- cppcache/include/geode/CacheableBuiltins.hpp
+++ cppcache/include/geode/CacheableBuiltins.hpp
@@ -562,12 +562,46 @@ class _GEODE_EXPORT CacheableArray : public Cacheable {
   }
 };
 
+/**
+ * An immutable wrapper for array of booleans that can serve as
+ * a distributable object for caching.
+ */
 using BooleanArray = CacheableArray<bool>;
+
+/**
+ * An immutable wrapper for array of wide-characters that can serve as
+ * a distributable object for caching.
+ */
 using CharArray = CacheableArray<char16_t>;
+
+/**
+ * An immutable wrapper for array of doubles that can serve as
+ * a distributable object for caching.
+ */
 using CacheableDoubleArray = CacheableArray<double>;
+
+/**
+ * An immutable wrapper for array of floats that can serve as
+ * a distributable object for caching.
+ */
 using CacheableFloatArray = CacheableArray<float>;
+
+/**
+ * An immutable wrapper for array of 16-bit integers that can serve as
+ * a distributable object for caching.
+ */
 using CacheableInt16Array = CacheableArray<int16_t>;
+
+/**
+ * An immutable wrapper for array of 32-bit integers that can serve as
+ * a distributable object for caching.
+ */
 using CacheableInt32Array = CacheableArray<int32_t>;
+
+/**
+ * An immutable wrapper for array of 64-bit integers that can serve as
+ * a distributable object for caching.
+ */
 using CacheableInt64Array = CacheableArray<int64_t>;
 
 _GEODE_CACHEABLE_ARRAY_TYPE_DEF_(std::shared_ptr<CacheableString>,
