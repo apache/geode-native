325e30f0bda73e793aff2a27d71b25d0e694d075
Mark Hanson <mhanson@pivotal.io>
2018-01-29 17:42:58 -0800

    GEODE-4335: Working example

--- examples/cpp/customserializer/main.cpp
+++ examples/cpp/customserializer/main.cpp
@@ -20,46 +20,144 @@
 // Include the Geode libraries.
 #include <geode/CacheFactory.hpp>
 #include <geode/PoolManager.hpp>
+#include <geode/PdxSerializer.hpp>
+#include <geode/PdxWrapper.hpp>
 
 // Use the "geode" namespace.
 using namespace apache::geode::client;
 
+static const std::string CLASSNAME = "com.example.Order";
+
+class Order {
+ public:
+  Order(unsigned long inOrderID, const std::string& inName,
+        unsigned int inQuantity)
+      : orderID(inOrderID), name(inName), quantity(inQuantity){};
+  ~Order(){};
+  unsigned long getOrderID() { return orderID; }
+  const std::string& getName() const { return name; }
+  unsigned int getQuantity() { return quantity; }
+  size_t getSize() const {
+    size_t size = name.length();
+    size += sizeof(orderID);
+    size += sizeof(quantity);
+    return size;
+  }
+  void print() {
+    std::cout << " OrderID: " << orderID << std::endl << " Product Name: " << name << std::endl << " Quantity: " << quantity << std::endl;
+  }
+
+ private:
+  unsigned long orderID;
+  std::string name;
+  unsigned int quantity;
+};
+
+class OrderSerializer : public PdxSerializer {
+ public:
+  OrderSerializer() {}
+
+  virtual ~OrderSerializer() {}
+
+  virtual void* fromData(const std::string& className, PdxReader& pdxReader) {
+    if (className != CLASSNAME) {
+      return nullptr;
+    }
+
+    try {
+      auto myOrder = new Order((unsigned long)pdxReader.readLong("objectid"),
+                               pdxReader.readString("name"),
+                               (unsigned int)pdxReader.readInt("quantity"));
+      return myOrder;
+    } catch (std::exception& e) {
+      std::cout << "Caught exception: " << e.what() << std::endl;
+      return nullptr;
+    }
+  }
+
+  static void deallocate(void* testObject, const std::string& className) {
+    if (className == CLASSNAME) {
+      Order* order = reinterpret_cast<Order*>(testObject);
+      delete order;
+    }
+  }
+
+  static size_t objectSize(const void* testObject,
+                           const std::string& className) {
+    size_t size = 0;
+    if (className == CLASSNAME) {
+      const Order* order = reinterpret_cast<const Order*>(testObject);
+      size = order->getSize();
+    }
+    return size;
+  }
+
+  virtual bool toData(void* userObject, const std::string& className,
+                      PdxWriter& pdxWriter) {
+    if (className != CLASSNAME) {
+      return false;
+    }
+
+    try {
+      auto myOrder = static_cast<Order*>(userObject);
+      pdxWriter.writeLong("orderid", myOrder->getOrderID());
+      pdxWriter.writeString("name", myOrder->getName());
+      pdxWriter.writeInt("quantity", myOrder->getQuantity());
+    } catch (std::exception& e) {
+      std::cout << "Caught exception: " << e.what() << std::endl;
+      return false;
+    }
+    return true;
+  }
+
+  UserDeallocator getDeallocator(const std::string& className) {
+    if (className == CLASSNAME) {
+      return deallocate;
+    }
+    return nullptr;
+  }
+
+  UserObjectSizer getObjectSizer(const std::string& className) {
+    if (className == CLASSNAME) {
+      return objectSize;
+    }
+    return NULL;
+  }
+};
 
 int main(int argc, char** argv) {
 
   auto cacheFactory = CacheFactory();
   cacheFactory.set("log-level", "none");
   auto cache = cacheFactory.create();
-
   auto poolFactory = cache.getPoolManager().createFactory();
+
   poolFactory->addLocator("localhost", 10334);
   auto pool = poolFactory->create("pool");
-
   auto regionFactory = cache.createRegionFactory(PROXY);
-
   auto region = regionFactory.setPoolName("pool").create("orders");
 
+  std::shared_ptr<PdxSerializer> orderSer =
+      std::shared_ptr<PdxSerializer>(new OrderSerializer());
+  cache.getTypeRegistry().registerPdxSerializer(orderSer);
+
+  auto order1 = new Order(1, "product x", 42);
+  std::shared_ptr<PdxWrapper> pdxobj1(
+      new PdxWrapper(order1, CLASSNAME, orderSer));
+  auto order2 = new Order(2, "product y", 37);
+  std::shared_ptr<PdxWrapper> pdxobj2(
+      new PdxWrapper(order2, CLASSNAME, orderSer));
 
   std::cout << "Storing orders in the region" << std::endl;
-  region->put("a123","1983");
-  region->put("a124","1982");
-
+  region->put("Customer1", pdxobj1);
+  region->put("Customer2", pdxobj2);
 
   std::cout << "Getting the orders from the region" << std::endl;
-  auto a123 = region->get("a123");
-  auto a124 = region->get("a124");
-  std::cout << "a123 = " << a123->toString() << std::endl;
-  std::cout << "a124 = " << a124->toString() << std::endl;
+  auto wrappedOrder =
+      std::dynamic_pointer_cast<PdxWrapper>(region->get("Customer1"));
+  auto* customer1Order = reinterpret_cast<Order*>(wrappedOrder->getObject());
 
-
-  std::cout << "Removing order a123's info from the region" << std::endl;
-  region->remove("a123");
-  if(region->existsValue("a123")) {
-    std::cout << "a123's info not deleted" << std::endl;
-  }
-  else {
-    std::cout << "a123's info successfully deleted" << std::endl;
-  }
+  customer1Order->print();
 
   cache.close();
 }
