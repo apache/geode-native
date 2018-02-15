d2309c694eee7e31563f04b1a7430148720f74e6
Ryan McMahon <rmcmahon@pivotal.io>
2018-02-15 11:16:59 -0800

    [DOC] Created and linked to dependency script

---
+++ packer/windows/install-dependencies.ps1
@@ -0,0 +1,10 @@
+choco install jdk8 -confirm
+choco install cmake.portable -confirm
+choco install git.install -confirm
+choco install activeperl -confirm
+choco install doxygen.install --allowEmptyChecksums -confirm
+choco install nunit.install --version 2.6.4 -confirm
+choco install netfx-4.5.2-devpack --allowEmptyChecksums -confirm
+choco install nsis -confirm
+choco install patch -confirm
+choco install gnuwin32-coreutils.portable -confirm
