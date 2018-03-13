4a4641ddab68bdffb74d2610abbfd1b931aae4a4
Ivan Godwin <igodwin@pivotal.io>
2018-03-13 16:41:54 -0700

    GEODE-4421: added support for clicache
    Signed-off-by: Mike Martell <mmartell@pivotal.io>

--- clicache/src/FunctionService.cpp
+++ clicache/src/FunctionService.cpp
@@ -45,7 +45,7 @@ namespace Apache
           
           auto nativeRegion = ((Region<TKey, TValue>^)rg)->GetNative();
           auto execution = native::FunctionService::onRegion(nativeRegion);
-          return Execution<TResult>::Create( execution, nullptr );
+          return Execution<TResult>::Create( std::move(execution), nullptr );
 
         _GF_MG_EXCEPTION_CATCH_ALL2/* due to auto replace */
       }
@@ -56,7 +56,7 @@ namespace Apache
         _GF_MG_EXCEPTION_TRY2/* due to auto replace */
 
           auto nativeptr = native::FunctionService::onServer(pl->GetNative());
-          return Execution<TResult>::Create( nativeptr , nullptr);
+          return Execution<TResult>::Create(std::move(nativeptr) , nullptr);
 
         _GF_MG_EXCEPTION_CATCH_ALL2/* due to auto replace */
       }
@@ -67,7 +67,7 @@ namespace Apache
         _GF_MG_EXCEPTION_TRY2/* due to auto replace */
 
           auto nativeptr = native::FunctionService::onServers(pl->GetNative());
-          return Execution<TResult>::Create( nativeptr , nullptr);
+          return Execution<TResult>::Create(std::move(nativeptr) , nullptr);
 
         _GF_MG_EXCEPTION_CATCH_ALL2/* due to auto replace */
       }
@@ -80,13 +80,13 @@ namespace Apache
           if(auto realCache = dynamic_cast<Cache^>(cache))
           {
             auto nativeptr = native::FunctionService::onServer(realCache->GetNative());
-            return Execution<TResult>::Create( nativeptr, nullptr );
+            return Execution<TResult>::Create(std::move(nativeptr), nullptr );
           }
           else
           {
             auto authCache = dynamic_cast<AuthenticatedCache^>(cache);
             auto nativeptr = native::FunctionService::onServer(authCache->GetNative());
-            return Execution<TResult>::Create( nativeptr, nullptr );
+            return Execution<TResult>::Create(std::move(nativeptr), nullptr );
           }
 
         _GF_MG_EXCEPTION_CATCH_ALL2/* due to auto replace */
@@ -100,13 +100,13 @@ namespace Apache
           if(auto realCache = dynamic_cast<Cache^>(cache))
           {
             auto nativeptr = native::FunctionService::onServers(realCache->GetNative());
-            return Execution<TResult>::Create( nativeptr, nullptr );
+            return Execution<TResult>::Create(std::move(nativeptr), nullptr );
           }
           else
           {
             auto authCache = dynamic_cast<AuthenticatedCache^>(cache);
             auto nativeptr = native::FunctionService::onServers(authCache->GetNative());
-            return Execution<TResult>::Create( nativeptr, nullptr );
+            return Execution<TResult>::Create(std::move(nativeptr), nullptr );
           }
 
         _GF_MG_EXCEPTION_CATCH_ALL2/* due to auto replace */
