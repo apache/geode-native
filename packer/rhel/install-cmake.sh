cd718763b512365e17cee166d7c1544f3b53513b
Jacob Barrett <jbarrett@pivotal.io>
2018-03-16 05:55:38 -0700

    GEODE-4850: Update linux and solaris packers with CMake 3.10.2

--- packer/rhel/install-cmake.sh
+++ packer/rhel/install-cmake.sh
@@ -19,7 +19,7 @@ set -x -e -o pipefail
 
 tmp=`mktemp`
 
-curl -o ${tmp} -v -L https://cmake.org/files/v3.5/cmake-3.5.0-Linux-x86_64.sh
+curl -o ${tmp} -v -L https://cmake.org/files/v3.10/cmake-3.10.2-Linux-x86_64.sh
 
 bash ${tmp} --skip-license --prefix=/usr/local
 
