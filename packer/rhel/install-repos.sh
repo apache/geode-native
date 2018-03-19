2242c29723b7731741aa421b8b78d29fd5c137d7
Michael Oleske <moleske@pivotal.io>
2018-03-19 16:10:07 -0700

    GEODE-4899: Use only RHEL 7

--- packer/rhel/install-repos.sh
+++ packer/rhel/install-repos.sh
@@ -6,9 +6,9 @@
 # The ASF licenses this file to You under the Apache License, Version 2.0
 # (the "License"); you may not use this file except in compliance with
 # the License.  You may obtain a copy of the License at
-# 
+#
 #      http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,6 +17,6 @@
 
 set -x -e -o pipefail
 
-yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm
-yum install -y https://rhel6.iuscommunity.org/ius-release.rpm
+yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+yum install -y https://rhel7.iuscommunity.org/ius-release.rpm
 
