c405537eb89b435adaf17b37e0ef7dcd49127aab
Michael Oleske <moleske@pivotal.io>
2018-04-17 10:44:05 -0700

    GEODE-4842: Fixing examples
    Signed-off-by: Ryan McMahon <rmcmahon@pivotal.io>

--- examples/dotnet/AuthInitialize/Program.cs
+++ examples/dotnet/AuthInitialize/Program.cs
@@ -31,7 +31,7 @@ namespace Apache.Geode.Examples.AuthInitialize
       var cache = cacheFactory.Create();
       var poolFactory = cache.GetPoolFactory()
           .AddLocator("localhost", 10334);
-      poolFactory.Create("pool", cache);
+      poolFactory.Create("pool");
       var regionFactory = cache.CreateRegionFactory(RegionShortcut.PROXY)
           .SetPoolName("pool");
       var region = regionFactory.Create<string, string>("region");
--- examples/dotnet/PdxAutoSerializer/Program.cs
+++ examples/dotnet/PdxAutoSerializer/Program.cs
@@ -34,7 +34,7 @@ namespace Apache.Geode.Examples.Serializer
 
             var poolFactory = cache.GetPoolFactory()
                 .AddLocator("localhost", 10334);
-            poolFactory.Create("pool", cache);
+            poolFactory.Create("pool");
 
             var regionFactory = cache.CreateRegionFactory(RegionShortcut.PROXY)
                 .SetPoolName("pool");
