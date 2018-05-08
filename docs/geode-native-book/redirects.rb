27725e2148045d961ea06cf69856a9efc511cde1
Dave Barnes <dbarnes@pivotal.io>
2018-05-08 14:59:12 -0700

    GEODE-4728: Docs - tweak redirects for server manual

--- docs/geode-native-book/redirects.rb
+++ docs/geode-native-book/redirects.rb
@@ -17,8 +17,7 @@
 r301 %r{/releases/latest/javadoc/(.*)}, 'http://geode.apache.org/releases/latest/javadoc/$1'
 
 # Links to User Guides #
-rewrite '/', '/docs/guide-native/17/about-client-users-guide.html'
-rewrite '/index.html', '/docs/guide-native/17/about-client-users-guide.html'
-r301 %r{/gfmanual/(.*)}, 'http://gemfire.docs.pivotal.io/docs-gemfire/$1'
+rewrite '/', '/docs/guide-native/15/about-client-users-guide.html'
+rewrite '/index.html', '/docs/guide-native/15/about-client-users-guide.html'
 r301 %r{/serverman/(.*)}, 'http://geode.apache.org/docs/guide/16/$1'
 r301 %r{/geodeman/(.*)}, 'http://geode.apache.org/docs/guide/16/$1'
