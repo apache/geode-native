095727ac3ae99a0df65e9d1cf8f2b5e15f5097cf
Dave Barnes <dbarnes@pivotal.io>
2018-07-05 12:04:27 -0700

    GEODE-4728: Docs - housekeeping - update build for development

--- docs/geode-native-book/redirects.rb
+++ docs/geode-native-book/redirects.rb
@@ -17,7 +17,7 @@
 r301 %r{/releases/latest/javadoc/(.*)}, 'http://geode.apache.org/releases/latest/javadoc/$1'
 
 # Links to User Guides #
-rewrite '/', '/docs/guide-native/15/about-client-users-guide.html'
-rewrite '/index.html', '/docs/guide-native/15/about-client-users-guide.html'
+rewrite '/', '/docs/guide-native/18/about-client-users-guide.html'
+rewrite '/index.html', '/docs/guide-native/18/about-client-users-guide.html'
 r301 %r{/serverman/(.*)}, 'http://geode.apache.org/docs/guide/16/$1'
 r301 %r{/geodeman/(.*)}, 'http://geode.apache.org/docs/guide/16/$1'
