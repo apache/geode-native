af9b53e846c4d77d3033d2b71d76314eb2ef1cca
Mike Martell <mmartell@pivotal.io>
2018-08-09 14:48:46 -0700

    Update clicache integration tests to use new DataSerializable interface.

--- clicache/integration-test/SerializationTestsN.cs
+++ clicache/integration-test/SerializationTestsN.cs
@@ -496,10 +496,13 @@ namespace Apache.Geode.Client.UnitTests
         get { return this.GetType().ToString(); }
     }
 
-    public Int32 ClassId
-    {
-        get { return this.GetType().ToString(); }
-    }
+    //public Int32 ClassId
+    //{
+    //  get
+    //  {
+    //    return 0x0;
+    //  }
+    //}
 
 
     #endregion
