d4d68d88d3757de847a73225216a8fe7ef265500
Mike Martell <mmartell@pivotal.io>
2018-08-30 11:22:40 -0700

    Switch back to std::type_info

--- cppcache/include/geode/DataSerializable.hpp
+++ cppcache/include/geode/DataSerializable.hpp
@@ -57,7 +57,7 @@ class APACHE_GEODE_EXPORT DataSerializable : public virtual Serializable {
    *
    * The type must be unique within an application suite.
    */
-  virtual const std::string getType() const { return typeid(*this).name(); }
+  const std::type_info& getType() const { return typeid(*this); }
 };
 
 }  // namespace client
