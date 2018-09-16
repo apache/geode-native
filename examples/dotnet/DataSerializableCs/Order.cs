14bf21e711e22d801c3aacf57dc41b71da17595d
Mike Martell <mmartell@pivotal.io>
2018-09-16 10:08:27 -0700

    GEODE-5259: Fix missing Type property in example code

--- examples/dotnet/DataSerializableCs/Order.cs
+++ examples/dotnet/DataSerializableCs/Order.cs
@@ -19,51 +19,56 @@ using System;
 
 namespace Apache.Geode.Examples.Serializer
 {
-    public class Order : IDataSerializable
+  public class Order : IDataSerializable
+  {
+    public int OrderId { get; set; }
+    public string Name { get; set; }
+    public short Quantity { get; set; }
+
+    // A default constructor is required for deserialization
+    public Order() { }
+
+    public Order(int orderId, string name, short quantity)
     {
-        public int OrderId { get; set; }
-        public string Name { get; set; }
-        public short Quantity { get; set; }
-
-        // A default constructor is required for deserialization
-        public Order() { }
-
-        public Order(int orderId, string name, short quantity)
-        {
-            OrderId = orderId;
-            Name = name;
-            Quantity = quantity;
-        }
-
-        public override string ToString()
-        {
-            return string.Format("Order: [{0}, {1}, {2}]", OrderId, Name, Quantity);
-        }
-
-        public void ToData(DataOutput output)
-        {
-            output.WriteInt32(OrderId);
-            output.WriteUTF(Name);
-            output.WriteInt16(Quantity);
-        }
-
-        public void FromData(DataInput input)
-        {
-            OrderId = input.ReadInt32();
-            Name = input.ReadUTF();
-            Quantity = input.ReadInt16();
-        }   
-
-        public ulong ObjectSize
-        {
-            get { return 0; }
-        }    
-        
-        public static ISerializable CreateDeserializable()
-        {
-            return new Order();
-        }
+      OrderId = orderId;
+      Name = name;
+      Quantity = quantity;
     }
+
+    public override string ToString()
+    {
+      return string.Format("Order: [{0}, {1}, {2}]", OrderId, Name, Quantity);
+    }
+
+    public void ToData(DataOutput output)
+    {
+      output.WriteInt32(OrderId);
+      output.WriteUTF(Name);
+      output.WriteInt16(Quantity);
+    }
+
+    public void FromData(DataInput input)
+    {
+      OrderId = input.ReadInt32();
+      Name = input.ReadUTF();
+      Quantity = input.ReadInt16();
+    }
+
+    public ulong ObjectSize
+    {
+      get { return 0; }
+    }
+
+    public String Type
+    {
+      get { return this.GetType().ToString(); }
+    }
+
+    public static ISerializable CreateDeserializable()
+    {
+      return new Order();
+    }
+  }
 }
 
 
