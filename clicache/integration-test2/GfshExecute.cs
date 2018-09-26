36887e6feb80c50a44fa05360d415892ad9afd28
Blake Bender <bbender@pivotal.io>
2018-09-26 18:30:14 +0000

    GEODE-5746: Refactor CLI integration-test2 to look like c++ version
    Co-authored-by: Ivan Godwin <igodwin@pivotal.io>

---
+++ clicache/integration-test2/GfshExecute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+using System.IO;
+using System.Net.Sockets;
+using Xunit;
+
+namespace Apache.Geode.Client.IntegrationTests
+{
+    public class GfshExecute : Gfsh
+    {
+        public override void execute(string cmd)
+        {
+            System.Diagnostics.Debug.WriteLine("Executing: " + cmd);
+        }
+    }
+}
