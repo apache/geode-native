d122aaf042e38046cf0778817ef0d4afc0cbfc65
Mike Martell <mmartell@pivotal.io>
2018-11-26 15:51:30 -0800

    Initial commit of functional .Net functionExecution example.

---
+++ examples/dotnet/FunctionExecutionCs/README.md
@@ -0,0 +1,27 @@
+﻿# PutGetRemove Example
+This is a very simple example showing how to create a `Cache` using the `CacheFactory`,
+configure a `Pool` with a `PoolFactory`, and configure a `Region` with a `RegionFactory`.
+We then put, get, and remove some primitive data in the region.
+
+## Prerequisites
+* Install [Apache Geode](https://geode.apache.org)
+* Build and install [Apache Geode Native](https://github.com/apache/geode-native)
+
+## Running
+* Start Geode Server and create region.
+  ```
+  gfsh>start locator --name=locator
+  gfsh>start server --name=server
+  gfsh>create region --name=example_userinfo --type=PARTITION
+  ```
+* Execute `Apache.Geode.Examples.PutGetRemove.exe`.
+  
+  output:
+  ```
+  Storing id and username in the region
+  Getting the user info from the region
+  rtimmons = Robert Timmons
+  scharles = Sylvia Charles
+  Removing rtimmons info from the region
+  Info for rtimmons has been deleted
+  ```
--- examples/dotnet/FunctionExecutionCs/stopserver.ps1
+++ examples/dotnet/FunctionExecutionCs/stopserver.ps1
@@ -1,4 +1,4 @@
-# Licensed to the Apache Software Foundation (ASF) under one or more
+﻿# Licensed to the Apache Software Foundation (ASF) under one or more
 # contributor license agreements.  See the NOTICE file distributed with
 # this work for additional information regarding copyright ownership.
 # The ASF licenses this file to You under the Apache License, Version 2.0
@@ -13,16 +13,27 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-cmake_minimum_required(VERSION 3.10)
+$GFSH_PATH = ""
+if (Get-Command gfsh -ErrorAction SilentlyContinue)
+{
+    $GFSH_PATH = "gfsh"
+}
+else
+{
+    if (-not (Test-Path env:GEODE_HOME))
+    {
+        Write-Host "Could not find gfsh.  Please set the GEODE_HOME path. e.g. "
+        Write-Host "(Powershell) `$env:GEODE_HOME = <path to Geode>"
+        Write-Host " OR"
+        Write-Host "(Command-line) set %GEODE_HOME% = <path to Geode>"
+    }
+    else
+    {
+        $GFSH_PATH = "$env:GEODE_HOME\bin\gfsh.bat"
+    }
+}
 
-project(@PRODUCT_DLL_NAME@.DotNet.Examples LANGUAGES NONE)
-
-add_subdirectory(AuthInitialize)
-add_subdirectory(ContinuousQueryCs)
-add_subdirectory(DataSerializableCs)
-add_subdirectory(FunctionExecutionCs)
-add_subdirectory(PdxAutoSerializer)
-add_subdirectory(PdxSerializableCs)
-add_subdirectory(PutGetRemove)
-add_subdirectory(RemoteQueryCs)
-
+if ($GFSH_PATH -ne "")
+{
+   Invoke-Expression "$GFSH_PATH -e 'connect' -e 'shutdown --include-locators=true'"
+}
