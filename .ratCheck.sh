f89a3d59a18f616cdc51b5df94be8fd2490444df
Blake Bender <ekalbredneb@hotmail.com>
2018-12-12 11:50:21 -0800

    GEODE-6139: Enforce Apache Rat findings in Travis CI (#414)
    * GEODE-6139: Fix problems reported by rat in source release
    - Add missing license headers to a couple of files
    - Fix bad line for '.vs' in .gitignore
    - Tell rat to ignore some trivial config files
    - Remove stray tmp file
    - Fix cmake file that was badly reformatted
    - Fix .cpackignore string.  .gitignore files use globbing, not regex, so they can't be read into cpack ignore directly
    - Run a script to execute rat and parse the result, rather than execute directly from Travis, so we can fail the build if rat reports issues
    
    Co-authored-by: Ernest Burghardt <eburghardt@pivotal.io>
    Co-authored-by: Mike Martell <mmartell@pivotal.io>
    Co-authored-by: Jacob Barrett <jbarrett@pivotal.io>

---
+++ .ratCheck.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+SOURCE_DIR=.
+COUNT=`java -jar /apache-rat-0.12/apache-rat-0.12.jar -E ${SOURCE_DIR}/.ratignore -d ${SOURCE_DIR} | grep '== File:' | sed 's/== File://' | wc -l`
+
+if [ $COUNT -gt 0 ]
+    then
+        echo "Rat check failed, $COUNT files are missing Apache license headers"
+fi
+
+exit $COUNT
+
