4034b3b3007e3a5feb5c988f6cee9afe543d5224
Ivan Godwin <igodwin@pivotal.io>
2018-12-27 11:05:11 -0800

    Execute Repair-AuthorizedKeyPermission on boot
    Co-authored-by: Mike Martell <mmartell@pivotal.io>

--- packer/windows/install-ssh.ps1
+++ packer/windows/install-ssh.ps1
@@ -22,3 +22,4 @@ write-host "Installing OpenSSH"
 choco install openssh -params '"/SSHServerFeature"' -confirm
 
 schtasks.exe /Create /TN init-ssh /RU SYSTEM /SC ONSTART /TR "powershell.exe -File 'C:\Program Files\Amazon\Ec2ConfigService\Scripts\init-ssh.ps1'"
+schtasks.exe /Create /TN fix-sshuser-permission /RU SYSTEM /SC ONSTART /TR "powershell.exe -File 'C:\Program Files\Amazon\Ec2ConfigService\Scripts\fix-sshuser-permission.ps1'"
