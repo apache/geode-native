7498d2549a62b18e8d89bef112fa70177dbbc9ff
Dave Barnes <dbarnes@pivotal.io>
2019-01-07 16:12:05 -0800

    Repair some bad links

--- docs/geode-native-book/redirects.rb
+++ docs/geode-native-book/redirects.rb
@@ -5,7 +5,7 @@
 #(the "License"); you may not use this file except in compliance with
 #the License.  You may obtain a copy of the License at
 #
-#http://www.apache.org/licenses/LICENSE-2.0
+#https://www.apache.org/licenses/LICENSE-2.0
 #
 #Unless required by applicable law or agreed to in writing, software
 #distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,10 +14,10 @@
 #permissions and limitations under the License.
 
 # Links to API Documentation #
-r301 %r{/releases/latest/javadoc/(.*)}, 'http://geode.apache.org/releases/latest/javadoc/$1'
+r301 %r{/releases/latest/javadoc/(.*)}, 'https://geode.apache.org/releases/latest/javadoc/$1'
 
 # Links to User Guides #
 rewrite '/', '/docs/geode-native/18/about-client-users-guide.html'
 rewrite '/index.html', '/docs/geode-native/18/about-client-users-guide.html'
-r301 %r{/serverman/(.*)}, 'http://geode.apache.org/docs/guide/18/$1'
-r301 %r{/geodeman/(.*)}, 'http://geode.apache.org/docs/guide/18/$1'
+r301 %r{/serverman/(.*)}, 'https://geode.apache.org/docs/guide/18/$1'
+r301 %r{/geodeman/(.*)}, 'https://geode.apache.org/docs/guide/18/$1'
