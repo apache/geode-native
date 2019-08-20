fa155b155ef3e8f8ed4319c0d7d622d11d4f2916
Mario Ivanac <mario.ivanac@est.tech>
2019-08-20 08:08:03 +0200

    GEODE-7086: Modify hostname check

--- cppcache/src/PoolFactory.cpp
+++ cppcache/src/PoolFactory.cpp
@@ -308,15 +308,13 @@ PoolFactory& PoolFactory::addCheck(const std::string& host, int port) {
   }
 
   ACE_INET_Addr addr(port, host.c_str());
-
   // check unknown host
   const int maxlength = 256;
   char char_array[maxlength];
   addr.get_host_name(char_array, maxlength);
-  if (host.compare(char_array) != 0) {
+  if (strstr(char_array, host.c_str()) == nullptr) {
     throw IllegalArgumentException("Unknown host " + host);
   }
-
   return *this;
 }
 
