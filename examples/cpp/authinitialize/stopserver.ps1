5da9c325330473f305b7b45eaf5986de6146ffee
Charlie Black <charliemblack@gmail.com>
2019-09-25 09:09:28 -0700

    GEODE-6781 - No reason to destroy the region if we are deleting the data directory.   Wait for process to die before trying to delete the data directories.

--- examples/cpp/authinitialize/stopserver.ps1
+++ examples/cpp/authinitialize/stopserver.ps1
@@ -32,10 +32,19 @@ else
         $GFSH_PATH = "$env:GEODE_HOME\bin\gfsh.bat"
     }
 }
+$locatorPid = Get-Content -Path locator/vf.gf.locator.pid
+$serverPid = Get-Content -Path server/vf.gf.server.pid
 
 if ($GFSH_PATH -ne "")
 {
    Invoke-Expression "$GFSH_PATH -e 'connect --locator=localhost[10334] --user=server --password=server' -e 'shutdown --include-locators=true'"
+}
+
+while(Get-Process -Id $locatorPid -ErrorAction SilentlyContinue) {
+	Start-Sleep -Seconds 0.5
+}
+while(Get-Process -Id $serverPid -ErrorAction SilentlyContinue) {
+	Start-Sleep -Seconds 0.5
 }
 
 Remove-Item -Path locator -Recurse -Force
