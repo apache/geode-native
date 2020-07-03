0d1ea60ac07471832336973ab07384e29c5408c1
Jacob Barrett <jbarrett@pivotal.io>
2020-07-02 20:27:11 -0700

    Makes SSH common on AWS and GCP

--- packer/windows/google/init-ssh.ps1
+++ packer/windows/google/init-ssh.ps1
@@ -21,29 +21,44 @@ param (
     [switch] $Schedule = $false
 )
 
-Set-Variable modulePath -Option Constant -Scope Local -Value (Join-Path $env:ProgramData -ChildPath "Amazon\EC2-Windows\Launch\Module\Ec2Launch.psd1")
+Set-Variable log -Option Constant -Scope Local -Value c:\ssh-init.log
 Set-Variable scriptPath -Option Constant -Scope Local -Value (Join-Path $PSScriptRoot -ChildPath $MyInvocation.MyCommand.Name)
 Set-Variable authorizedKeysPath -Option Constant -Scope Local -Value (Join-Path $env:ProgramData -ChildPath "ssh\administrators_authorized_keys")
+Set-Variable sskKeysPath -Option Constant -Scope Local -Value (Join-Path $env:ProgramData -ChildPath "ssh\ssh-keys")
 
-Import-Module $modulePath
-
-Initialize-Log -Filename "Ec2Launch.log" -AllowLogToConsole
+function Write-Log {
+    Write-Host "$args"
+    Add-Content -Path $log -Value "$args"
+}
 
 if ($Schedule) {
   Write-Log "Scheduling SSH Authorized Keys Initialization..."
-  Register-ScriptScheduler -ScriptPath $scriptPath -ScheduleName "SSH Authorized Keys Initialization"
-  Write-Log "Scheduling SSH Authorized Keys Initialization."
-
-  Complete-Log
+  schtasks.exe /Create /F /TN "SSH Authorized Keys Initialization" /RU SYSTEM /SC ONSTART /TR "powershell.exe -File '$scriptPath'"
+  Write-Log "Scheduled SSH Authorized Keys Initialization."
   Exit 0
 }
 
 Write-Log "Initializing SSH Authorized Keys..."
 
-Invoke-WebRequest -Uri "http://169.254.169.254/latest/meta-data/public-keys/0/openssh-key" -OutFile $authorizedKeysPath
-icacls $authorizedKeysPath /inheritance:r /grant "SYSTEM:(F)" /grant "BUILTIN\Administrators:(F)"
+Invoke-WebRequest -Headers @{'Metadata-Flavor'='Google'} -Uri 'http://metadata.google.internal/computeMetadata/v1/instance/attributes/ssh-keys' -OutFile "$sskKeysPath"
+Get-Content -Path "$sskKeysPath" -OutVariable sshKeys
+
+Write-Log "Got ssh-keys: $sshKeys"
+
+Remove-Item -Path "$authorizedKeysPath" -Force
+$sshKeys = $sshKeys.Split([Environment]::NewLine, [System.StringSplitOptions]::RemoveEmptyEntries)
+foreach ($sshKey in $sshKeys) {
+    $sshKey = $sshKey.Split(':')
+    $user = $sshKey[0]
+    $key = $sshKey[1]
+
+    Write-Log "Adding admin user $user with key $key."
+
+    New-LocalUser -AccountNeverExpires -Name $user -NoPassword
+    Add-LocalGroupMember -Group "Administrators" -Member $user
+
+    Add-Content -Path "$authorizedKeysPath" -Value "$key"
+    icacls "${authorizedKeysPath}" /inheritance:r /grant "SYSTEM:(F)" /grant "BUILTIN\Administrators:(F)"
+}
 
 Write-Log "Initialized SSH Authorized Keys."
-
-Complete-Log
-Exit 0
