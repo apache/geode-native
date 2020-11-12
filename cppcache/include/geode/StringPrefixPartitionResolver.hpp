2a89c3a791e7b5c8c64618539fd6e2d58e6129eb
Mario Salazar de Torres <mario.salazar.de.torres@est.tech>
2020-11-12 11:49:23 +0100

    GEODE-8702: Add StringPrefixPartitionResolver
     - Added StringPrefixPartitionResolver as it is included on the Java API
     - UTs were added to verify the PR functionality.

---
+++ cppcache/include/geode/StringPrefixPartitionResolver.hpp
@@ -0,0 +1,110 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#ifndef GEODE_STRINGPREFIXPARTITIONRESOLVER_H_
+#define GEODE_STRINGPREFIXPARTITIONRESOLVER_H_
+
+#include "PartitionResolver.hpp"
+
+namespace apache {
+namespace geode {
+namespace client {
+
+class CacheableKey;
+class EntryEvent;
+
+/**
+ * Implement the <code>PartitionResolver</code> interface to enable custom
+ * partitioning on the <code>PartitionedRegion</code>.
+ *
+ * The Key class or callback arg can implement the PartitionResolver interface
+ * to enable custom partitioning, or you can configure your own
+ * PartitionResolver class in partition attributes (for instance when the Key is
+ * a primitive type or String):
+ * - Implement the appropriate equals. For all implementations, you need to be
+ * sure to code the class equals method so it properly verifies equality for the
+ * PartitionResolver implementation. This might mean verifying that class names
+ * are the same or that the returned routing objects are the same etc. When you
+ * initiate the partitioned region on multiple nodes, Geode uses the equals
+ * method to ensure you are using the same PartitionResolver implementation for
+ * all of the nodes for the region.
+ * - Geode uses the routing object's hashCode to determine where the data is
+ * being managed. Say, for example, you want to colocate all Trades by month and
+ * year. The key is implemented by a TradeKey class which also implements the
+ * PartitionResolver interface:
+ *
+ * @code
+ * public class TradeKey implements PartitionResolver {
+ *   private String tradeID;
+ *   private Month month;
+ *   private Year year;
+ *
+ *   public TradingKey() { }
+ *   public TradingKey(Month month, Year year) {
+ *     this.month = month;
+ *     this.year = year;
+ *   }
+ *   public Serializable getRoutingObject(EntryOperation opDetails) {
+ *     return this.month + this.year;
+ *   }
+ * }
+ * @endcode
+ *
+ * In the example above, all trade entries with the same month and year are
+ * guaranteed to be colocated.
+ */
+class APACHE_GEODE_EXPORT StringPrefixPartitionResolver
+    : public PartitionResolver {
+ public:
+  StringPrefixPartitionResolver();
+  explicit StringPrefixPartitionResolver(std::string delimiter);
+
+  StringPrefixPartitionResolver(const StringPrefixPartitionResolver&) = delete;
+
+  /**
+   * @brief destructor
+   */
+  ~StringPrefixPartitionResolver() override = default;
+
+  void operator=(const StringPrefixPartitionResolver&) = delete;
+
+  /**
+   * Returns the name of the PartitionResolver
+   * @return String name
+   */
+  const std::string& getName() override;
+
+  /**
+   * @param opDetails the detail of the entry event
+   * @throws RuntimeException - any exception thrown will terminate the
+   * operation and the exception will be passed to the calling thread.
+   * @return object associated with entry event which allows the Partitioned
+   * Region to store associated data together
+   */
+  std::shared_ptr<CacheableKey> getRoutingObject(
+      const EntryEvent& opDetails) override;
+
+ protected:
+  std::string delimiter_;
+};
+}  // namespace client
+}  // namespace geode
+}  // namespace apache
+
+#endif  // GEODE_STRINGPREFIXPARTITIONRESOLVER_H_
