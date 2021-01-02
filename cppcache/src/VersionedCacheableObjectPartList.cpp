d147586d9b8a95066d8fd71928a5bb204f6c96dc
Jacob Barrett <jbarrett@pivotal.io>
2021-01-02 08:47:34 -0800

    clang-format fixes

--- cppcache/src/VersionedCacheableObjectPartList.cpp
+++ cppcache/src/VersionedCacheableObjectPartList.cpp
@@ -233,7 +233,9 @@ void VersionedCacheableObjectPartList::fromData(DataInput& input) {
           versionTag->setInternalMemID(ids.at(idNumber));
           break;
         }
-        default: { break; }
+        default: {
+          break;
+        }
       }
       m_versionTags[index] = versionTag;
     }
