87746ef8e4c20671b1bc34114147a3d6b1f2d758
Mike Martell <mmartell@pivotal.io>
2021-08-25 18:18:25 -0700

    GEODE-9464: More formatting

--- netcore/AspNetCore GeodeSession Sample/Middleware/HttpContextItemsMiddleware.cs
+++ netcore/AspNetCore GeodeSession Sample/Middleware/HttpContextItemsMiddleware.cs
@@ -19,34 +19,27 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 
-namespace SessionSample.Middleware
-{
-    #region snippet1
-    public class HttpContextItemsMiddleware
-    {
-        private readonly RequestDelegate _next;
-        public static readonly object HttpContextItemsMiddlewareKey = new Object();
+namespace SessionSample.Middleware {
+#region snippet1
+  public class HttpContextItemsMiddleware {
+    private readonly RequestDelegate _next;
+    public static readonly object HttpContextItemsMiddlewareKey = new Object();
 
-        public HttpContextItemsMiddleware(RequestDelegate next)
-        {
-            _next = next;
-        }
-
-        public async Task Invoke(HttpContext httpContext)
-        {
-            httpContext.Items[HttpContextItemsMiddlewareKey] = "K-9";
-
-            await _next(httpContext);
-        }
+    public HttpContextItemsMiddleware(RequestDelegate next) {
+      _next = next;
     }
 
-    public static class HttpContextItemsMiddlewareExtensions
-    {
-        public static IApplicationBuilder 
-            UseHttpContextItemsMiddleware(this IApplicationBuilder app)
-        {
-            return app.UseMiddleware<HttpContextItemsMiddleware>();
-        }
+    public async Task Invoke(HttpContext httpContext) {
+      httpContext.Items[HttpContextItemsMiddlewareKey] = "K-9";
+
+      await _next(httpContext);
     }
-    #endregion
+  }
+
+  public static class HttpContextItemsMiddlewareExtensions {
+    public static IApplicationBuilder UseHttpContextItemsMiddleware(this IApplicationBuilder app) {
+      return app.UseMiddleware<HttpContextItemsMiddleware>();
+    }
+  }
+#endregion
 }
