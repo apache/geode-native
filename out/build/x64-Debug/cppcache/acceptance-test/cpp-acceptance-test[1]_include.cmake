53d8fffa44adef96ab72601f7754ad413ae00736
Mike Martell <mmartell@pivotal.io>
2021-11-11 12:15:05 -0800

    Remove the bogus out folder

--- out/build/x64-Debug/cppcache/acceptance-test/cpp-acceptance-test[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "C:/geode-native/out/build/x64-Debug/cppcache/acceptance-test/cpp-acceptance-test[1]_tests.cmake")
-  include("C:/geode-native/out/build/x64-Debug/cppcache/acceptance-test/cpp-acceptance-test[1]_tests.cmake")
-else()
-  add_test(cpp-acceptance-test_NOT_BUILT cpp-acceptance-test_NOT_BUILT)
-endif()
