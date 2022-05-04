7f3dec72004a5768cdaa32ae5e9acf2590ffae53
Mario Salazar de Torres <mario.salazar.de.torres@est.tech>
2022-05-04 13:30:35 +0200

    GEODE-10276: Fixed UTs
     - Removed PdxInstance UTs which are not needed anymore, as the method
     updateStream doesn't exist anymore.
      - Modified tests to work accordingly with code changes.

--- cppcache/test/PdxTypeTest.cpp
+++ cppcache/test/PdxTypeTest.cpp
@@ -22,7 +22,6 @@
 #include "DataOutputInternal.hpp"
 #include "PdxType.hpp"
 #include "PdxTypeRegistry.hpp"
-#include "PdxTypes.hpp"
 
 namespace {
 using apache::geode::client::ByteArray;
@@ -31,180 +30,124 @@ using apache::geode::client::DataOutputInternal;
 using apache::geode::client::PdxFieldTypes;
 using apache::geode::client::PdxType;
 using apache::geode::client::PdxTypeRegistry;
-using apache::geode::client::PdxTypes;
 
 const std::string gemfireJsonClassName = "__GEMFIRE_JSON";
 
 class PdxTypeTest : public ::testing::Test, public ByteArrayFixture {};
 
 TEST_F(PdxTypeTest, testTwoObjectsWithSameClassnameAndSameFieldsAreEquals) {
-  PdxTypeRegistry pdxTypeRegistry(nullptr);
+  PdxType pdxType1{gemfireJsonClassName};
+  pdxType1.addField("bar0", PdxFieldTypes::STRING);
+  pdxType1.addField("bar1", PdxFieldTypes::STRING);
 
-  PdxType m_pdxType1(pdxTypeRegistry, gemfireJsonClassName, false);
-  PdxType m_pdxType2(pdxTypeRegistry, gemfireJsonClassName, false);
+  PdxType pdxType2{gemfireJsonClassName};
+  pdxType2.addField("bar0", PdxFieldTypes::STRING);
+  pdxType2.addField("bar1", PdxFieldTypes::STRING);
 
-  m_pdxType1.addVariableLengthTypeField("bar0", "string",
-                                        PdxFieldTypes::STRING);
-  m_pdxType1.addVariableLengthTypeField("bar1", "string",
-                                        PdxFieldTypes::STRING);
-  m_pdxType2.addVariableLengthTypeField("bar0", "string",
-                                        PdxFieldTypes::STRING);
-  m_pdxType2.addVariableLengthTypeField("bar1", "string",
-                                        PdxFieldTypes::STRING);
-
-  EXPECT_TRUE(m_pdxType1 == m_pdxType2);
+  EXPECT_TRUE(pdxType1 == pdxType2);
 }
 
-TEST_F(PdxTypeTest,
-       testTwoObjectsWithSameClassnameAndSameFieldsInDifferentOrderAreEquals) {
-  PdxTypeRegistry pdxTypeRegistry(nullptr);
+TEST_F(
+    PdxTypeTest,
+    testTwoObjectsWithSameClassnameAndSameFieldsInDifferentOrderAreDifferent) {
+  PdxType pdxType1{gemfireJsonClassName};
+  pdxType1.addField("bar1", PdxFieldTypes::STRING);
+  pdxType1.addField("bar0", PdxFieldTypes::STRING);
 
-  PdxType m_pdxType1(pdxTypeRegistry, gemfireJsonClassName, false);
-  PdxType m_pdxType2(pdxTypeRegistry, gemfireJsonClassName, false);
-  m_pdxType1.addVariableLengthTypeField("bar1", "string",
-                                        PdxFieldTypes::STRING);
-  m_pdxType1.addVariableLengthTypeField("bar0", "string",
-                                        PdxFieldTypes::STRING);
-  m_pdxType2.addVariableLengthTypeField("bar0", "string",
-                                        PdxFieldTypes::STRING);
-  m_pdxType2.addVariableLengthTypeField("bar1", "string",
-                                        PdxFieldTypes::STRING);
+  PdxType pdxType2{gemfireJsonClassName};
+  pdxType2.addField("bar0", PdxFieldTypes::STRING);
+  pdxType2.addField("bar1", PdxFieldTypes::STRING);
 
-  EXPECT_TRUE(m_pdxType1 == m_pdxType2);
+  EXPECT_FALSE(pdxType1 == pdxType2);
 }
 
 TEST_F(PdxTypeTest,
        testTwoObjectsWithDifferentClassnameButSameFieldsAreNotEquals) {
-  PdxTypeRegistry pdxTypeRegistry(nullptr);
+  PdxType pdxType1{gemfireJsonClassName};
+  pdxType1.addField("bar0", PdxFieldTypes::STRING);
+  pdxType1.addField("bar1", PdxFieldTypes::STRING);
 
-  PdxType m_pdxType1(pdxTypeRegistry, gemfireJsonClassName, false);
-  PdxType m_pdxType2(pdxTypeRegistry, "otherClassName", false);
+  PdxType pdxType2{"otherClassName"};
+  pdxType2.addField("bar0", PdxFieldTypes::STRING);
+  pdxType2.addField("bar1", PdxFieldTypes::STRING);
 
-  m_pdxType1.addVariableLengthTypeField("bar0", "string",
-                                        PdxFieldTypes::STRING);
-  m_pdxType1.addVariableLengthTypeField("bar1", "string",
-                                        PdxFieldTypes::STRING);
-  m_pdxType2.addVariableLengthTypeField("bar0", "string",
-                                        PdxFieldTypes::STRING);
-  m_pdxType2.addVariableLengthTypeField("bar1", "string",
-                                        PdxFieldTypes::STRING);
-
-  EXPECT_FALSE(m_pdxType1 == m_pdxType2);
+  EXPECT_FALSE(pdxType1 == pdxType2);
 }
 
 TEST_F(PdxTypeTest, testTwoObjectsWithSameFieldsHaveTheSameHash) {
-  PdxTypeRegistry pdxTypeRegistry(nullptr);
+  PdxType pdxType1{gemfireJsonClassName};
+  pdxType1.addField("bar0", PdxFieldTypes::STRING);
+  pdxType1.addField("bar1", PdxFieldTypes::STRING);
+  pdxType1.addField("bar2", PdxFieldTypes::STRING);
 
-  PdxType m_pdxType1(pdxTypeRegistry, gemfireJsonClassName, false);
-  PdxType m_pdxType2(pdxTypeRegistry, gemfireJsonClassName, false);
-
-  m_pdxType1.addVariableLengthTypeField("bar0", "string",
-                                        PdxFieldTypes::STRING);
-  m_pdxType1.addVariableLengthTypeField("bar1", "string",
-                                        PdxFieldTypes::STRING);
-  m_pdxType1.addVariableLengthTypeField("bar2", "string",
-                                        PdxFieldTypes::STRING);
-
-  m_pdxType2.addVariableLengthTypeField("bar0", "string",
-                                        PdxFieldTypes::STRING);
-  m_pdxType2.addVariableLengthTypeField("bar1", "string",
-                                        PdxFieldTypes::STRING);
-  m_pdxType2.addVariableLengthTypeField("bar2", "string",
-                                        PdxFieldTypes::STRING);
+  PdxType pdxType2{gemfireJsonClassName};
+  pdxType2.addField("bar0", PdxFieldTypes::STRING);
+  pdxType2.addField("bar1", PdxFieldTypes::STRING);
+  pdxType2.addField("bar2", PdxFieldTypes::STRING);
 
   std::hash<PdxType> type1Hash;
   std::hash<PdxType> type2Hash;
-
-  EXPECT_EQ(type1Hash(m_pdxType1), type2Hash(m_pdxType2));
+  EXPECT_EQ(type1Hash(pdxType1), type2Hash(pdxType2));
 }
 
 TEST_F(PdxTypeTest, testTwoObjectsWithDifferentFieldsHaveDifferentHash) {
-  PdxTypeRegistry pdxTypeRegistry(nullptr);
+  PdxType pdxType1{gemfireJsonClassName};
+  pdxType1.addField("bar0", PdxFieldTypes::STRING);
+  pdxType1.addField("bar1", PdxFieldTypes::STRING);
 
-  PdxType m_pdxType1(pdxTypeRegistry, gemfireJsonClassName, false);
-  PdxType m_pdxType2(pdxTypeRegistry, gemfireJsonClassName, false);
-
-  m_pdxType1.addVariableLengthTypeField("bar0", "string",
-                                        PdxFieldTypes::STRING);
-  m_pdxType1.addVariableLengthTypeField("bar1", "string",
-                                        PdxFieldTypes::STRING);
-
-  m_pdxType2.addVariableLengthTypeField("bar2", "string",
-                                        PdxFieldTypes::STRING);
-  m_pdxType2.addVariableLengthTypeField("bar3", "string",
-                                        PdxFieldTypes::STRING);
+  PdxType pdxType2{gemfireJsonClassName};
+  pdxType2.addField("bar2", PdxFieldTypes::STRING);
+  pdxType2.addField("bar3", PdxFieldTypes::STRING);
 
   std::hash<PdxType> type1Hash;
   std::hash<PdxType> type2Hash;
 
-  EXPECT_NE(type1Hash(m_pdxType1), type2Hash(m_pdxType2));
+  EXPECT_NE(type1Hash(pdxType1), type2Hash(pdxType2));
 }
 
 TEST_F(PdxTypeTest,
        testTwoObjectsWithSameFieldsInDifferentOrderHaveTheSameHash) {
-  PdxTypeRegistry pdxTypeRegistry(nullptr);
+  PdxType pdxType1{gemfireJsonClassName};
+  pdxType1.addField("bar0", PdxFieldTypes::STRING);
+  pdxType1.addField("bar1", PdxFieldTypes::STRING);
+  pdxType1.addField("bar2", PdxFieldTypes::STRING);
 
-  PdxType m_pdxType1(pdxTypeRegistry, gemfireJsonClassName, false);
-  PdxType m_pdxType2(pdxTypeRegistry, gemfireJsonClassName, false);
-
-  m_pdxType1.addVariableLengthTypeField("bar0", "string",
-                                        PdxFieldTypes::STRING);
-  m_pdxType1.addVariableLengthTypeField("bar1", "string",
-                                        PdxFieldTypes::STRING);
-  m_pdxType1.addVariableLengthTypeField("bar2", "string",
-                                        PdxFieldTypes::STRING);
-
-  m_pdxType2.addVariableLengthTypeField("bar1", "string",
-                                        PdxFieldTypes::STRING);
-  m_pdxType2.addVariableLengthTypeField("bar2", "string",
-                                        PdxFieldTypes::STRING);
-  m_pdxType2.addVariableLengthTypeField("bar0", "string",
-                                        PdxFieldTypes::STRING);
+  PdxType pdxType2{gemfireJsonClassName};
+  pdxType2.addField("bar1", PdxFieldTypes::STRING);
+  pdxType2.addField("bar2", PdxFieldTypes::STRING);
+  pdxType2.addField("bar0", PdxFieldTypes::STRING);
 
   std::hash<PdxType> type1Hash;
   std::hash<PdxType> type2Hash;
 
-  EXPECT_EQ(type1Hash(m_pdxType1), type2Hash(m_pdxType2));
+  EXPECT_EQ(type1Hash(pdxType1), type2Hash(pdxType2));
 }
 
 TEST_F(PdxTypeTest,
        testTwoObjectsWithSameFieldsNamesButDifferentTypesHaveDifferentHash) {
-  PdxTypeRegistry pdxTypeRegistry(nullptr);
+  PdxType pdxType1{gemfireJsonClassName};
+  pdxType1.addField("bar0", PdxFieldTypes::STRING);
+  pdxType1.addField("bar1", PdxFieldTypes::STRING);
+  pdxType1.addField("bar2", PdxFieldTypes::STRING);
 
-  PdxType m_pdxType1(pdxTypeRegistry, gemfireJsonClassName, false);
-  PdxType m_pdxType2(pdxTypeRegistry, gemfireJsonClassName, false);
-
-  m_pdxType1.addVariableLengthTypeField("bar0", "string",
-                                        PdxFieldTypes::STRING);
-  m_pdxType1.addVariableLengthTypeField("bar1", "string",
-                                        PdxFieldTypes::STRING);
-  m_pdxType1.addVariableLengthTypeField("bar2", "string",
-                                        PdxFieldTypes::STRING);
-
-  m_pdxType2.addVariableLengthTypeField("bar0", "string",
-                                        PdxFieldTypes::STRING);
-  m_pdxType2.addFixedLengthTypeField("bar1", "bool", PdxFieldTypes::BOOLEAN,
-                                     PdxTypes::kPdxBooleanSize);
-  m_pdxType2.addFixedLengthTypeField("bar2", "int", PdxFieldTypes::INT,
-                                     PdxTypes::kPdxIntegerSize);
+  PdxType pdxType2{gemfireJsonClassName};
+  pdxType2.addField("bar0", PdxFieldTypes::STRING);
+  pdxType2.addField("bar1", PdxFieldTypes::BOOLEAN);
+  pdxType2.addField("bar2", PdxFieldTypes::INT);
 
   std::hash<PdxType> type1Hash;
   std::hash<PdxType> type2Hash;
-
-  EXPECT_NE(type1Hash(m_pdxType1), type2Hash(m_pdxType2));
+  EXPECT_NE(type1Hash(pdxType1), type2Hash(pdxType2));
 }
 
 TEST_F(PdxTypeTest, testSerializeJavaPdxType) {
-  PdxTypeRegistry pdx_type_registry{nullptr};
-  PdxType pdx_type{pdx_type_registry, gemfireJsonClassName, false};
-
-  pdx_type.addVariableLengthTypeField("foo", "", PdxFieldTypes::STRING);
-  pdx_type.addVariableLengthTypeField("alice", "", PdxFieldTypes::STRING);
-  pdx_type.addFixedLengthTypeField("bar1", "", PdxFieldTypes::BOOLEAN,
-                                   PdxTypes::kPdxBooleanSize);
+  PdxType pdxType{gemfireJsonClassName};
+  pdxType.addField("foo", PdxFieldTypes::STRING);
+  pdxType.addField("alice", PdxFieldTypes::STRING);
+  pdxType.addField("bar1", PdxFieldTypes::BOOLEAN);
 
   DataOutputInternal out;
-  pdx_type.toData(out);
+  pdxType.toData(out);
 
   EXPECT_BYTEARRAY_EQ(
       "2D2B2A00256F72672E6170616368652E67656F64652E7064782E696E7465726E"
@@ -216,16 +159,13 @@ TEST_F(PdxTypeTest, testSerializeJavaPdxType) {
 }
 
 TEST_F(PdxTypeTest, testSerializeNoJavaPdxType) {
-  PdxTypeRegistry pdx_type_registry{nullptr};
-  PdxType pdx_type{pdx_type_registry, gemfireJsonClassName, false, false};
-
-  pdx_type.addVariableLengthTypeField("foo", "", PdxFieldTypes::STRING);
-  pdx_type.addVariableLengthTypeField("alice", "", PdxFieldTypes::STRING);
-  pdx_type.addFixedLengthTypeField("bar1", "", PdxFieldTypes::BOOLEAN,
-                                   PdxTypes::kPdxBooleanSize);
+  PdxType pdxType{gemfireJsonClassName, false};
+  pdxType.addField("foo", PdxFieldTypes::STRING);
+  pdxType.addField("alice", PdxFieldTypes::STRING);
+  pdxType.addField("bar1", PdxFieldTypes::BOOLEAN);
 
   DataOutputInternal out;
-  pdx_type.toData(out);
+  pdxType.toData(out);
 
   EXPECT_BYTEARRAY_EQ(
       "2D2B2A00256F72672E6170616368652E67656F64652E7064782E696E7465726E"
@@ -237,43 +177,37 @@ TEST_F(PdxTypeTest, testSerializeNoJavaPdxType) {
 }
 
 TEST_F(PdxTypeTest, testDeserializeJavaPdxType) {
-  PdxTypeRegistry pdx_type_registry{nullptr};
-  PdxType pdx_expected{pdx_type_registry, gemfireJsonClassName, false};
-
-  pdx_expected.addVariableLengthTypeField("foo", "", PdxFieldTypes::STRING);
-  pdx_expected.addVariableLengthTypeField("alice", "", PdxFieldTypes::STRING);
-  pdx_expected.addFixedLengthTypeField("bar1", "", PdxFieldTypes::BOOLEAN,
-                                       PdxTypes::kPdxBooleanSize);
+  PdxType pdxExpected{gemfireJsonClassName};
+  pdxExpected.addField("foo", PdxFieldTypes::STRING);
+  pdxExpected.addField("alice", PdxFieldTypes::STRING);
+  pdxExpected.addField("bar1", PdxFieldTypes::BOOLEAN);
 
   DataOutputInternal out;
-  pdx_expected.toData(out);
+  pdxExpected.toData(out);
 
-  auto pdx_type = std::dynamic_pointer_cast<PdxType>(
-      PdxType::CreateDeserializable(pdx_type_registry));
+  auto pdxType =
+      std::dynamic_pointer_cast<PdxType>(PdxType::createDeserializable());
   DataInputInternal in(out.getBuffer(), out.getBufferLength());
-  pdx_type->fromData(in);
+  pdxType->fromData(in);
 
-  EXPECT_EQ(pdx_expected, *pdx_type);
+  EXPECT_EQ(pdxExpected, *pdxType);
 }
 
 TEST_F(PdxTypeTest, testDeserializeNoJavaPdxType) {
-  PdxTypeRegistry pdx_type_registry{nullptr};
-  PdxType pdx_expected{pdx_type_registry, gemfireJsonClassName, false, false};
-
-  pdx_expected.addVariableLengthTypeField("foo", "", PdxFieldTypes::STRING);
-  pdx_expected.addVariableLengthTypeField("alice", "", PdxFieldTypes::STRING);
-  pdx_expected.addFixedLengthTypeField("bar1", "", PdxFieldTypes::BOOLEAN,
-                                       PdxTypes::kPdxBooleanSize);
+  PdxType pdxExpected{gemfireJsonClassName, false};
+  pdxExpected.addField("foo", PdxFieldTypes::STRING);
+  pdxExpected.addField("alice", PdxFieldTypes::STRING);
+  pdxExpected.addField("bar1", PdxFieldTypes::BOOLEAN);
 
   DataOutputInternal out;
-  pdx_expected.toData(out);
+  pdxExpected.toData(out);
 
-  auto pdx_type = std::dynamic_pointer_cast<PdxType>(
-      PdxType::CreateDeserializable(pdx_type_registry));
+  auto pdxType =
+      std::dynamic_pointer_cast<PdxType>(PdxType::createDeserializable());
   DataInputInternal in(out.getBuffer(), out.getBufferLength());
-  pdx_type->fromData(in);
+  pdxType->fromData(in);
 
-  EXPECT_EQ(pdx_expected, *pdx_type);
+  EXPECT_EQ(pdxExpected, *pdxType);
 }
 
 }  // namespace
